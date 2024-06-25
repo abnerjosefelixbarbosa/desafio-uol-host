255177d9f253d7634bab59215022e21a0d802ad6
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2024-06-25 15:12:13 -0300

    commited by Abner

--- frontend-angular/src/app/pages/player-list/player-list.component.ts
+++ frontend-angular/src/app/pages/player-list/player-list.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-player-list',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [
+    MatDividerModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatListModule,
+  ],
   templateUrl: './player-list.component.html',
   styleUrl: './player-list.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PlayerListComponent {}
